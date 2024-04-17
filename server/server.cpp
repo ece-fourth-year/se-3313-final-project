42231efcb03823a12cf9a2cbb7a82a52aa5e8f36
d4n13lx13 <danxie2001@gmail.com>
2024-04-16 22:21:29 -0400

    server additional code

--- server/server.cpp
+++ server/server.cpp
@@ -1,7 +1,9 @@
-#include <stdio.h>
+#include <iostream>
+#include <vector>
 #include <thread>
 #include "../include/socketserver.h"
 #include "../include/Semaphore.h"
+#include <string.h>
 
 /*
 struct {
@@ -35,6 +37,7 @@ struct GameSession{
 
 void threadSession(GameSession gameSem);
 void clientHandlerThread(Semaphore *gameSem, Client *client, int *answer, bool *clientGuessedCorrectly, int *portFirst);
+void timerThread(GameSession *gameSession);
 
 /*
 main func {
@@ -77,17 +80,63 @@ timerThread func (gameSession) {
     
 } */
 
-void threadSession(GameSession gameSession) {
 
-    Client *client1 = &gameSession.player1;
-    Client *client2 = &gameSession.player2;
+int main(void) {
+
+    vector<thread> threadSessions;
+    bool timerStarted = false;
+    thread timerThread;
+
+    SocketServer server = SocketServer(2000);
+
+    while (1) {
+
+        GameSession *newGameSession;
+        Socket clientSocket(server.Accept());
+        Client *client;
+        client->socket = clientSocket;
+\
+        if (!timerStarted) {
+            newGameSession->player1 = *client;
+            thread timerTh(timerThread, newGameSession);
+            timerStarted = true;
+        } else {
+            newGameSession->player2 = *client;
+            timerThread.join();
+            timerStarted = false;
+            threadSessions.push_back(thread(threadSession, newGameSession));
+        }
+
+    
+    }
+
+}
+
+void timerThread(GameSession *gameSession) {
+
+    int timeElapsed = 0;
+
+    while (timeElapsed < 60) {
+        if (gameSession->player2.port != 0) {
+            return;
+        }
+        timeElapsed++;
+    }
+
+    gameSession->player1.socket.Close();
+}
+
+void threadSession(GameSession *gameSession) {
+
+    Client& client1 = gameSession->player1;
+    Client& client2 = gameSession->player2;
 
     // start game message  
     string initGameMsg = "Init";
     // start game 1
-    client1->socket.Write(ByteArray(initGameMsg));
+    client1.socket.Write(ByteArray(initGameMsg));
     // start game 2
-    client2->socket.Write(ByteArray(initGameMsg));
+    client2.socket.Write(ByteArray(initGameMsg));
 
     int answer = rand() % 10 + 1;
     bool player1Correct = false;
@@ -95,32 +144,38 @@ void threadSession(GameSession gameSession) {
     int portFirst = -1;
     Semaphore gameSem = Semaphore("gameSem", 0, true);
 
-    clientHandlerThread(&gameSem, client1, &answer, &player1Correct, &portFirst);
-    clientHandlerThread(&gameSem, client2, &answer, &player2Correct, &portFirst);
+    thread client1Thread(clientHandlerThread, &gameSem, client1, &answer, &player1Correct, &portFirst);
+    thread client2Thread(clientHandlerThread, &gameSem, client2, &answer, &player2Correct, &portFirst);
+
+    client1Thread.join();
+    client2Thread.join();
 
     if (player1Correct && player2Correct) {
-        if (portFirst == client1->port) {
+        if (portFirst == client1.port) {
             // player 1 wins
-            client1->socket.Write(ByteArray("You win!"));
-            client2->socket.Write(ByteArray("You lose!"));
+            client1.socket.Write(ByteArray("You win!"));
+            client2.socket.Write(ByteArray("You lose!"));
         } else {
             // player 2 wins
-            client1->socket.Write(ByteArray("You lose!"));
-            client2->socket.Write(ByteArray("You win!"));
+            client1.socket.Write(ByteArray("You lose!"));
+            client2.socket.Write(ByteArray("You win!"));
         }
     } else if (player1Correct && !player2Correct) {
         // player 1 wins
-        client1->socket.Write(ByteArray("You win!"));
-        client2->socket.Write(ByteArray("You lose!"));
+        client1.socket.Write(ByteArray("You win!"));
+        client2.socket.Write(ByteArray("You lose!"));
     } else if (player2Correct && !player1Correct) {
         // player 2 wins
-        client1->socket.Write(ByteArray("You lose!"));
-        client2->socket.Write(ByteArray("You win!"));
+        client1.socket.Write(ByteArray("You lose!"));
+        client2.socket.Write(ByteArray("You win!"));
     } else {
         // both lose
-        client1->socket.Write(ByteArray("You lose!"));
-        client2->socket.Write(ByteArray("You lose!"));
+        client1.socket.Write(ByteArray("You lose!"));
+        client2.socket.Write(ByteArray("You lose!"));
     }
+
+    client1.socket.Close();
+    client2.socket.Close();
 
 }
 
@@ -147,11 +202,24 @@ clientHandlerThread func (gameSem, client, answer, clientGuessedCorrectly, portF
 void clientHandlerThread(Semaphore *gameSem, Client *client, int *answer, bool *clientGuessedCorrectly, int *portFirst) {
 
     // on data from client
-    // TODO: implement getting data from client
+
+    ByteArray buffer;
+
+    Socket clientSocket = client->socket;
+
+    int clientGuess;
+
+    // Wait for a message from the client
+    while (clientSocket.Read(buffer) == 0)
+    {
+        sleep(1);
+    }
 
     gameSem->Wait();
 
-    if (*answer == 1) {
+    clientGuess = stoi(buffer.ToString());
+
+    if (*answer == clientGuess) {
         *clientGuessedCorrectly = true;
 
         if (*portFirst == -1) {
